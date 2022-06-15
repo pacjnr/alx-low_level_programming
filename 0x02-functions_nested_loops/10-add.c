ce465f5a67fe65f53079696b2ce24e220f171854
pacjnr <pacojunior91@gmail.com>
2022-06-15 16:05:31 -0700

    loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add- adds the interges
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: returns the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
